1e782acaee2484cd28e78d43e6c0574a02b4a1dd
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-03-03 19:43:31 -0300

    Bug fixes.

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,11 +2,9 @@
 
 USER=`echo $SETTINGS | jq -r .user.value`
 PASSWORD=`echo $SETTINGS | jq -r .password.value`
-PORT=`echo $SETTINGS | jq -r .port.value`
 
 export USER
 export PASSWORD
-export PORT
 
 $BIN_DIR/child-install.sh
 
